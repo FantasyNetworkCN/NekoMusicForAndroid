d68b02b383892e97c1e0bfcae32a1325252977d4
NyaNyagulugulu <support@cnmsb.xin>
2026-05-16 14:38:57 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.neko.music"
         minSdk = 23
         targetSdk = 36
-        versionCode = 51
-        versionName = "20260514"
+        versionCode = 52
+        versionName = "20260516"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
