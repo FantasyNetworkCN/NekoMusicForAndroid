111fc28dcd225522e8dd9e2856afab41736e98c4
NyaNyagulugulu <support@cnmsb.xin>
2026-05-22 19:31:48 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "com.neko.music"
         minSdk = 23
         targetSdk = 36
-        versionCode = 53
+        versionCode = 54
         versionName = "20260522"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
